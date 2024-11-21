5c73be172e1ecb3c3e7b8f6f81ee565962e23125
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-11-21 21:36:27 +0000

    Updated logic in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-  import { ref } from 'vue';
+  import { ref, computed, onMounted, watch } from 'vue';
   import GraphGrids from './components/GraphGrids.vue';
   import Header from './components/Header.vue';
   import Footer from './components/Footer.vue';
@@ -7,24 +7,36 @@
 
   const pokemonName = ref('ditto');
 
-  const URL = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`;
+  const pokemonData = ref(null);
 
-  const getPokemon = async () => {
-    const respnse = await URL.get();
-    return respnse.data;
-  }
+  const URL = computed(() => `https://pokeapi.co/api/v2/pokemon/${pokemonName.value}`);
+
+  const fetchPokemonData = async () => {
+    try {
+      const respnse = await axios.get(URL.value);
+      pokemonData.value = respnse.data;
+      console.log("Fetched Pokémon:", pokemonData.value);
+    } catch (err) {
+      console.console.error("Error fetching Pokemon data: ", err);
+      pokemonData.value = null;
+    }
+  };
+
+  onMounted(() => {
+    fetchPokemonData();
+  }),
+
+  watch(pokemonName, fetchPokemonData);
 
 </script>
 
 <template>
   <Header :pokemonName="pokemonName" />
-  <form action="">
-    <input type="text" placeholder="Choose your pokemon" :value="pokemonName" />
+  <form @submit.prevent="fetchPokemonDAta">
+    <input type="text" placeholder="Choose your pokemon.." :v-model="pokemonName" />
+    <button type="submit">Submit</button>
   </form>
+  <p>{{ pokemonName }}</p>
   <GraphGrids :pokemonName="pokemonName" />
   <Footer />
 </template>
-
-<style scoped>
-
-</style>
