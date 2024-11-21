83c76fd34452a98f57f2eb83ff4bc8d442e8896f
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-11-21 16:20:47 +0000

    Updated Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,3 +1,19 @@
 <template>
-  <h3>I am a footer</h3>
+  <footer class="p-4">
+    <h3 class="text-center">Coded by Luna Smith</h3>
+    <div class="flex align-middle justify-center gap-2">
+      <a 
+      class="py-2 px-4 hover:bg-gray-50 font-bold rounded-md " 
+      href="https://github.com/Lu-Smith/vue_tailwind_api_graphs" 
+      target="_blank">
+        GitHub
+      </a>
+      <a 
+      class="py-2 px-4 hover:bg-gray-50 font-bold rounded-md"
+      href="https://www.lunasmithart.com/" 
+      target="_blank">
+        Portfolio
+      </a>
+    </div>
+  </footer>
 </template>
