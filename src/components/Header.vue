3be582cdcc83c1460e65ae09993cdb587934b6d7
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-11-21 21:36:58 +0000

    Passed props to Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,6 +4,7 @@
       <div class="flex lg:flex-1">
         <a href="#" class="-m-1.5 p-1.5">
           <span class="sr-only">Graph Blast</span>
+          <p>Welcome, {{ pokemonName }}!</p>
           <img class="h-8 w-auto" src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600" alt="pokemon">
         </a>
       </div>
@@ -141,6 +142,8 @@
 
 <script setup lang="ts">
 import { ref } from 'vue';
+
+defineProps(['pokemonName'])
 const isMenuVisible = ref(false);
 
 const toggleFlyoutMenu = () => {
