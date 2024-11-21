8767efc7114c25c544e965217663c47de5487928
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-11-21 20:13:19 +0000

    Added axios to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,27 @@
 <script setup lang="ts">
+  import { ref } from 'vue';
   import GraphGrids from './components/GraphGrids.vue';
   import Header from './components/Header.vue';
   import Footer from './components/Footer.vue';
+  import axios from "axios";
+
+  const pokemonName = ref('ditto');
+
+  const URL = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`;
+
+  const getPokemon = async () => {
+    const respnse = await URL.get();
+    return respnse.data;
+  }
+
 </script>
 
 <template>
-  <Header />
-  <GraphGrids />
+  <Header :pokemonName="pokemonName" />
+  <form action="">
+    <input type="text" placeholder="Choose your pokemon" :value="pokemonName" />
+  </form>
+  <GraphGrids :pokemonName="pokemonName" />
   <Footer />
 </template>
 
